674ad2c6c303e100aeaee9745a4fcb074ffc185b
Yael Widmann <yael.widmann@widas.de>
2015-06-03 18:45:35 +0200

    solve

--- Yael/shop/src/components/artikelverwaltung/warenkorb/warenkorbResource.ts
+++ Yael/shop/src/components/artikelverwaltung/warenkorb/warenkorbResource.ts
@@ -1,3 +1,17 @@
 /**
  * Created by yaelwidmann on 22.05.15.
  */
+import{Component,bootstrap,View, For} from 'angular2/angular2';
+
+import{Artikel} from '../Artikel';
+
+export class WarenkorbResource{
+    artikel: Array<Artikel>;
+    constructor(){
+        this.artikel=new Array<Artikel>();
+    }
+    addArtikel(artikel){
+        this.artikel.push(artikel);
+    }
+
+}
