dd35f627bcd17748d9778f851a244da701c25bc8
Sally Pfeifer <pfsa1012@hs-karlsruhe.de>
2015-06-04 13:05:51 +0200

    gulpfile & package.json jshint, changed + imagemin

--- Mirco/Router/gulpfile.js
+++ Mirco/Router/gulpfile.js
@@ -3,6 +3,12 @@
 var gulp = require('gulp')
 var typescript = require('gulp-tsc');
 var webserver = require('gulp-webserver');
+
+var jshint = require('gulp-jshint');
+
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+
  
 gulp.task('typescript', function(){
   gulp.src(['src/**/*.ts'])
@@ -23,3 +29,23 @@ gulp.task('webserver', function() {
 });
 
 gulp.task('default', ['typescript','webserver']);
+
+// JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+gulp.task('jshint', function() {
+  gulp.src('./src/scripts/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+
+});
+
+//changed task (Only pass through changed files)
+gulp.task('imagemin', function() {
+  var imgSrc = './src/images/**/*',
+      imgDst = './build/images';
+
+  gulp.src(imgSrc)
+      .pipe(changed(imgDst))
+      .pipe(imagemin())
+      .pipe(gulp.dest(imgDst));
+});
