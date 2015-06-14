958b8f28e7035a26e207e01d7ec2269774a6a6db
Yael Widmann <yael.widmann@widas.de>
2015-06-14 16:09:46 +0200

    warenkorb fehler behoben (ausßer imports)

--- Mirco/Router/src/warenkorb/warenkorbResource.ts
+++ Mirco/Router/src/warenkorb/warenkorbResource.ts
@@ -6,59 +6,55 @@ import {Warenkorb} from './warenkorb';
 
 
 var warenkorb: Array<Warenkorb> = [
-    new Warenkorb(new Artikel('Tisch',10),4), 
-    new Warenkorb(new Artikel('Sofa',10),3), 
-    new Warenkorb(new Artikel('Stuhl', 8),1)
+    new Warenkorb(new Artikel("Tisch", 10), 4) ,
+    new Warenkorb(new Artikel("Sofa", 10), 3) ,
+    new Warenkorb(new Artikel("Stuhl", 8), 1)
 ];
 
 export let WarenkorbResource: any = {
 
-    add(art: Artikel): void{
+    add(art: Artikel): void {
         let position = this.notIn(art);
-        if(position===-1){
-            warenkorb.push(new Warenkorb(art,1));
-        }
-        else{
+        if ( position === -1) {
+            warenkorb.push(new Warenkorb(art, 1));
+        } else {
             warenkorb[position].anzahl++;
         }
     },
 
-    delete(artikel: Artikel):void{
+    delete(artikel: Artikel): void {
 
         let position = this.notIn(artikel);
-        if (position !== -1){
+        if (position !== -1) {
             let anzahl = warenkorb[position].anzahl;
-            if (anzahl !== 1){
+            if (anzahl !== 1) {
                 warenkorb[position].anzahl--;
-            }
-            else{
-                warenkorb.splice(position,1);
+            } else {
+                warenkorb.splice(position, 1);
             }
         }
     },
-    
-    getGesamtpreis(): number{
+
+    getGesamtpreis(): number {
         let i = 0;
         let gespreis = 0;
-        for (i;i<warenkorb.length; i++){
+        for (i; i < warenkorb.length; i++) {
             gespreis = gespreis + warenkorb[i].artikel.preis * warenkorb[i].anzahl;
         }
         return gespreis;
     },
-    getWarenkorb(): Array<Warenkorb>{
+    getWarenkorb(): Array<Warenkorb> {
         return warenkorb;
     },
-    
-    notIn(artikel: Artikel): number{
-        for (let i = 0; i < warenkorb.length; i++){
-            if (warenkorb[i].artikel.name === artikel.name){
+    notIn(artikel: Artikel): number {
+        for (let i = 0; i < warenkorb.length; i++) {
+            if (warenkorb[i].artikel.name === artikel.name) {
                 return i;
             }
         }
         return -1;
     },
-    
-    deleteAll(): void{
+    deleteAll(): void {
         warenkorb = [];
-    }
+    };
 }
